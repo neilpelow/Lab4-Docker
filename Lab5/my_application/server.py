d96ad15e1f24d421866a10f5ba2de38e8d872522
= <neilpelow@gmail.com>
2015-10-14 12:44:51 -0400

    Lab5 Updated1

--- Lab5/my_application/server.py
+++ Lab5/my_application/server.py
@@ -1,9 +1,24 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
 def hello():
-    return "Hello World!"
+    return 'Hello World'
+
+@app.route('/user/<username>')
+def showUser(username):
+        return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+  
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0",debug=True)
+
